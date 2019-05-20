ed6b22e9ad06569460fc340b9fc067b3453e245d
krskelton <kskelton82@gmail.com>
2019-05-20 15:37:12 -0600

    Update caesar.py

--- caesar.py
+++ caesar.py
@@ -1,5 +1,26 @@
 from helpers import alphabet_position, rotate_character
 
 def encrypt(text, rot):
-    #Add your code here
-    return
+    #create a new empty string
+    return_string = ''
+    #for loop to go through each character in the message
+    for char in text:
+        #check if the character is an alpha character
+        if char.isalpha() == False:
+            #if not, just add it to the string
+            return_string += char
+        else:
+            #if it is an alpha character call the rotate_character function
+            return_string += rotate_character(char, rot)
+    #return the new string
+    return return_string
+
+
+def main():
+    # main code (input and print statements)
+    message = input("Type your message: ")
+    rotation = int(input("How much should each letter be rotated: "))
+    print(encrypt(message, rotation))
+
+if __name__ == "__main__":
+    main()
